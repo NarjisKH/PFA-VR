6625e5dc6198187fa65aeaf6d69cf49e53e9d293
NarjisKH <narjiskh08@gmail.com>
2022-11-24 14:10:40 +0100

    first commit

---
+++ Assets/changeColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    //     Behaviour halo=(Behaviour)GetComponent("Halo");
+    //     halo.Size=10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
