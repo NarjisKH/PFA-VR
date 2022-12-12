81d3ad111b3ca72d779d105b4608e8aeae8f9899
BENALI OULAHCEN Ikram <105321428+Karanosi@users.noreply.github.com>
2022-12-12 14:12:42 +0100

    added buttons

---
+++ Assets/Scripts/ComposantsInstantiateScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComposantsInstantiateScript : MonoBehaviour
+{
+    public int limitObject;
+    int nbrOfObject;
+    public GameObject objet;
+    public GameObject parent;
+    void start(){
+        nbrOfObject = 0;
+    }
+    public void ObjectInstantiateClick(){
+        if(nbrOfObject < limitObject){
+            ObjectInstantiate();
+            nbrOfObject++;
+        }
+        else
+        {
+            Debug.Log("Max de l'objet atteint");
+        }
+    }
+    public void ObjectInstantiate(){
+        Instantiate(objet,parent.transform);
+    }
+}
